db1e4b1bef14e5a8aa32d48f1c7a4eab949cdce0
Roberto Koryde <roberto.danna97@icloud.com>
2022-03-24 16:07:31 +0100

    Initial Commit

---
+++ PopOver/PopOverApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopOverApp.swift
+//  PopOver
+//
+//  Created by Roberto D'Anna on 24/03/22.
+//
+
+import SwiftUI
+
+@main
+struct PopOverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
